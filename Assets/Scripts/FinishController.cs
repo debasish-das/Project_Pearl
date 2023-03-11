11b05f0dc6c940e9b5028820d1820ad56ae79b5b
Debasish Das <debasish.das64@yahoo.com>
2023-03-10 23:23:44 -0800

    start and end scene, death from height

--- Assets/Scripts/FinishController.cs
+++ Assets/Scripts/FinishController.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishController : MonoBehaviour
 {
     private AudioSource _finishAudio;
+    private bool _isLevelComplete = false;
 
     private void Start()
     {
@@ -14,15 +15,23 @@ public class FinishController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player") 
+        if (collision.gameObject.name == "Player" && !_isLevelComplete)
         {
             _finishAudio.Play();
-
+            _isLevelComplete = true;
+            Invoke("CompleteLevel", 1.5f);
         }
     }
 
     private void CompleteLevel()
     {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
 
+        //Debug.Log(nextSceneIndex);
+
+        //if (nextSceneIndex < SceneManager.sceneCount)
+        //{
+        SceneManager.LoadScene(nextSceneIndex);
+        //}
     }
 }
